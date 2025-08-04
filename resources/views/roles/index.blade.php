1297aa00f5840220f38da4eee0fc4d9f9a577e1a
Osvaldo <osvaldosv.gobti@gmail.com>
2025-08-03 19:26:45 -0600

    comentarios

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -33,19 +33,9 @@
                         <td>{{ ++$i }}</td>
                         <td>{{ $role->name }}</td>
                         <td>
-                            {{-- <a class="btn btn-info btn-sm" href="{{ route('roles.show',$role->id) }}"><i class="fa-solid fa-list"></i> Ver</a> --}}
                             @can('role-edit')
                             <a class="btn btn-primary btn-sm" href="{{ route('roles.edit',$role->id) }}"> Editar</a>
                             @endcan
-
-                            {{-- @can('role-delete')
-                            <form method="POST" action="{{ route('roles.destroy', $role->id) }}" style="display:inline">
-                                @csrf
-                                @method('DELETE')
-
-                                <button type="submit" class="btn btn-danger btn-sm"><i class="fa-solid fa-trash"></i> Eliminar</button>
-                            </form>
-                            @endcan --}}
                         </td>
                     </tr>
                     @endforeach
